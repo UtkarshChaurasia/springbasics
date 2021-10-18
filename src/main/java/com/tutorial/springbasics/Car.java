bec8d0b129f6aa4605f8b120a0a9c99665fc8ca8
UtkarshChaurasia <msdianuc07@gmail.com>
2021-10-18 23:21:11 +0530

    annotation based configs

--- src/main/java/com/tutorial/springbasics/Car.java
+++ src/main/java/com/tutorial/springbasics/Car.java
@@ -1,5 +1,8 @@
 package com.tutorial.springbasics;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Car implements Vehicle {
 	public void drive() {
 		System.out.println("Chal rahi hai..");
